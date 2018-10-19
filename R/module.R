9f4bc11cec3d968411c023a72d6097fea1c364f1
hb <hb@aroma-project.org>
2018-10-19 16:54:00 -0700

    First working draft

--- R/module.R
+++ R/module.R
@@ -1,48 +1,151 @@
 #' @importFrom utils file_test
 #' @export
-modulepath <- function() {
+modulepath <- function(drop = TRUE, normalize = TRUE, unique = TRUE) {
   modulepath <- envvar("MODULEPATH")
   paths <- unlist(strsplit(modulepath, split = ":", fixed = TRUE))
+  if (unique) paths <- unique(paths)
+  if (drop) paths <- paths[file_test("-d", paths)]
+  if (normalize) paths <- unname(sapply(paths, FUN = normalizePath))
   paths
+}
+
+#' @export
+modulepath_repos_path <- function(must_work = TRUE) {
+  paths <- modulepath()
+  ## RULE: Module repository modules are located in a folder named repos/
+  paths2 <- paths[basename(paths) == "repos"]
+  if (must_work) {
+    if (length(paths2) == 0) {
+      stop(sprintf("No 'repos/' folder in MODULEPATH (%s)", paste(sQuote(paths), collapse = ", ")))
+    } else if (length(paths2) > 1) {
+      stop(sprintf("More than one 'repos/' folder in MODULEPATH (%s): %s", paste(sQuote(paths), collapse = ", "), paste(sQuote(paths2), collapse = ", ")))
+    }
+  }
+  paths2
 }
 
 
 #' @importFrom utils file_test
 #' @export
-modulepath_list_repos <- function(paths = modulepath()) {
-  res <- list()
-  for (path in paths) {
-    stopifnot(file_test("-d", path))
-    path <- normalizePath(path)
-    reposes <- dir(path = path, include.dirs = TRUE, no.. = TRUE, full.names = TRUE)
-    reposes <- reposes[file_test("-d", reposes)]
-    names(reposes) <- basename(reposes)
-    
-    res[[path]] <- reposes
-  }
+modulepath_find_all_repos <- function(path = modulepath_repos_path(), drop = TRUE) {
+  reposes <- dir(path = path, pattern = "[.]lua$", include.dirs = FALSE, no.. = TRUE, full.names = TRUE)
+  reposes <- reposes[file_test("-f", reposes)]
+  if (length(reposes) == 0) return(character(0L))
+
+  parent <- dirname(path)
+  paths <- c(setdiff(modulepath(), path), parent)
   
+  names <- sub("[.]lua$", "", basename(reposes))
+
+  res <- NULL
+  for (name in names) {
+    dir <- find_dir(name, paths = paths, must_work = FALSE)
+    if (length(dir) == 0) {
+      if (name == "Personal") {
+        dir <- "~/modulefiles"
+      }	else {
+        ## FIXME: TIPCC hardcoded module repositories
+        if (name == "Legacy-Scyld") {
+          dir <- "/opt/scyld/modulefiles"
+        } else if (name == "Legacy-etc") {
+          dir <- "/etc/modulefiles"
+        } else {
+          dir <- find_dir(name, paths = paths)
+        }
+      }
+    }
+    names(dir) <- name
+    res <- c(res, dir)
+  }
+
+  if (drop) res <- res[file_test("-d", res)]
+
   res
 }
 
 
-#' @importFrom utils file_test
 #' @export
-modulepath_find_repos <- function(repos, must_work = TRUE, paths = modulepath()) {
-  for (path in paths) {
-    stopifnot(file_test("-d", path))
-    path <- normalizePath(path)
-    reposes <- dir(path = path, include.dirs = TRUE, no.. = TRUE, full.names = TRUE)
-    reposes <- reposes[file_test("-d", reposes)]
-    names(reposes) <- basename(reposes)
-    idx <- which(repos == names(reposes))[1]
-    if (!is.na(idx)) return(repos[idx])
+modulepath_find_repos <- function(repos, must_work = TRUE) {
+  paths <- modulepath_find_all_repos()
+  path <- paths[repos]
+  if (is.na(path)) {
+    if (must_work) {
+      stop(sprintf("No such module repository in MODULEPATH (%s): %s", paste(sQuote(modulepath), collapse = ", "), repos))
+    }
+    return(NULL)
+  }
+  path
+}
+
+
+#' @export
+modulepath_list_repos <- function(repos, types = c("lmod"), rename = TRUE, format = c("tibble", "data.frame", "raw")) {
+  format <- match.arg(format)
+  path <- modulepath_find_repos(repos)
+
+  res <- list()
+  for (type in types) {
+    modules <- lmod_spider_modules(path)
+
+    if (rename) {
+      ## Rename fields
+      fields <- names(modules)
+      fields[fields == "defaultVersionName"] <- "default_version"
+      names(modules) <- fields
+      modules <- reorder_by_name(modules, c("package", "description", "url", "default_version", "versions"))
+      
+      modules <- modules[fields]
+    }
+
+    res[[type]] <- modules
   }
 
-  if (must_work) {
-    stop(sprintf("No such module in MODULEPATH (%s): %s", paste(sQuote(paths), collapse = ", "), repos))
+  if (format == "raw") return(res)
+
+  for (type in names(res)) {
+    modules <- res[[type]]
+
+    ns <- sapply(modules, FUN = length)
+    stopifnot(all(ns == ns[1]))
+
+    modules <- as.data.frame(modules)
+    rownames(modules) <- NULL
+
+    versions <- vector("list", length = nrow(modules))
+    names(versions) <- modules$package
+    help <- character(nrow(modules))
+    
+    version_data <- modules[["versions"]]
+    defaults <- modules[["default_version"]]
+    for (kk in seq_along(version_data)) {
+      versions[[kk]] <- unique(trim(version_data[[kk]]$versionName))
+      help_kk <- version_data[[kk]]$help
+      if (length(help_kk) > 1L) {
+        default <- which(versions[[kk]] == defaults[[kk]])
+##	str(list(versions_kk = versions[[kk]], default = default, help_kk = help_kk))
+	if (length(default) == 0) default <- 1L
+	help_kk <- help_kk[default]
+      }
+      help[kk] <- help_kk
+    }
+
+    modules[["versions"]] <- versions
+    modules[["help"]] <- help
+    
+    url <- modules[["url"]]
+    url <- lapply(strsplit(url, split = "[,]"), FUN = trim)
+    modules[["url"]] <- url
+
+    modules <- reorder_by_name(modules, c("package", "help", "description", "url", "default_version", "versions"))
+
+    if (format == "tibble") {
+      modules <- tibble::as_tibble(modules)
+    }
+    
+    res[[type]] <- modules
   }
-  
-  NULL
+
+  res
 }
 
 
@@ -63,9 +166,7 @@ lmod_spider <- function(args, ...) {
 #' @importFrom utils file_test
 #' @importFrom jsonlite fromJSON
 #' @export
-lmod_module_avail <- function(repos) {
-  path <- modulepath_find_repos(repos, must_work = TRUE)
-  
+lmod_spider_modules <- function(path) {
   json <- lmod_spider(args = c("-o jsonSoftwarePage", path))
   x <- fromJSON(json)
   
--- R/utils.R
+++ R/utils.R
@@ -20,3 +20,70 @@ envvar <- function(name, default = NA_character_) {
       stop(sprintf("Environment variable %s is not set", sQuote(name)))
   value
 }
+
+
+
+#' @importFrom utils file_test
+#' @export
+find_dir <- function(name, paths, must_work = TRUE) {
+  pattern <- sprintf("^%s$", name)
+  for (path in paths) {
+    dirs <- dir(path = path, pattern = pattern, include.dirs = FALSE, no.. = TRUE, full.names = TRUE)
+    dirs <- dirs[file_test("-d", dirs)]
+    if (length(dirs) > 0) return(dirs)
+  }
+  if (must_work) {
+    stop(sprintf("No such folder in paths (%s): %s", paste(sQuote(paths), collapse = ", "), name))
+  }
+  NULL
+}
+
+
+#' @export
+reorder_by_name <- function(x, preferred) {
+  names <- intersect(preferred, names(x))
+  names <- c(names, setdiff(names(x), names))
+  x[names]
+}
+
+
+#' @export
+as_html_table_tds <- function(x, collapse = "\n") {
+  html <- NULL
+  for (kk in seq_len(nrow(x))) {
+    module <- x[kk, ]
+
+    module <- as.list(module)
+    versions <- unlist(module$versions)
+    idx <- which(versions == module$default_version)
+    versions[idx] <- sprintf("<em>%s</em>", versions[idx])
+    versions <- paste(versions, collapse = ", ")
+
+    tds <- NULL
+    
+    body <- c("<strong>", module$package, "</strong>", "<br>", versions)
+    tds <- c(tds, "<td>", body, "</td>")
+
+    body <- c("<strong>", as_html(module$help), "</strong>", "<br>")
+    body <- c(body, as_html(module$description), "<br>")
+    body <- c(body, as_html(paste(unlist(module$url), collapse = ", ")))
+    tds <- c(tds, "<td>", body, "</td>")
+
+    html_kk <- c("<tr>", tds, "</tr>")
+    html <- c(html, html_kk)
+  } # for (kk in ...)
+
+  html <- paste(html, collapse = collapse)
+  
+  html
+}
+
+#' @export
+as_html_table <- function(x, label = "module &amp; versions", collapse = "\n") {
+  ths <- c("<td>", label, "</td>", "<td>", "description", "</td>")
+  hdr <- c("<tr>", ths, "</tr>")
+  rows <- as_html_table_tds(x, collapse = NULL)
+  html <- c("<table>", hdr, rows, "</table>")
+  html <- paste(html, collapse = collapse)
+  html
+}
